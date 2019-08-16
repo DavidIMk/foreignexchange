340dec00d6499f98a4233b76433643bcc2a30e66
DavidIMk <davidimmanuelmaksi@gmail.com>
2019-08-17 00:03:12 +0700

    Updates

---
+++ precache-manifest.a5a85706a6ff3aaf87b26f4e2d990bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b8acb3cc93f9285e5d3dd22b86a690",
+    "url": "/foreignexchange/index.html"
+  },
+  {
+    "revision": "57278550841b8c4d4938",
+    "url": "/foreignexchange/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "06c21a8aa00e12e638e5",
+    "url": "/foreignexchange/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "57278550841b8c4d4938",
+    "url": "/foreignexchange/static/js/2.5f8e27fa.chunk.js"
+  },
+  {
+    "revision": "06c21a8aa00e12e638e5",
+    "url": "/foreignexchange/static/js/main.24a84839.chunk.js"
+  },
+  {
+    "revision": "570b65bc681a37c0d4ff",
+    "url": "/foreignexchange/static/js/runtime~main.5058322a.js"
+  }
+]);
